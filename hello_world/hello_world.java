17d009c9475e32f82dd93a5054bb0ff26ec09c02
squallks <gamesrdc@gmail.com>
2016-10-03 19:07:46 +0200

    Ricardv.1

--- hello_world/hello_world.java
+++ hello_world/hello_world.java
@@ -1,2 +1,2 @@
 public static void main(String[] args) {
-	System.out.println("Hello world: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT.");
+	System.out.println("Hello world: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT.");

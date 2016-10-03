ad822dee0865c65de15bfd40260b1ce1429f0bb8
squallks <gamesrdc@gmail.com>
2016-10-03 19:00:21 +0200

    RicardDiv1

--- hello_world/hello_world.java
+++ hello_world/hello_world.java
@@ -1,2 +1,2 @@
 public static void main(String[] args) {
-	System.out.println("Hello world from Java!");
+	System.out.println("Hello world: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT.");
